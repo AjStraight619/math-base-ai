632b7f6ce9f48032b2ce62896cf1bc568df1ea14
Alex Straight <compsciaj@outlook.com>
2024-02-28 11:21:15 -0800

    added suspense boundry around useSearchParams hook

--- components/sidebar/sidebar-dashboard.tsx
+++ components/sidebar/sidebar-dashboard.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { Suspense } from "react";
 import Search from "./search";
 
 const SidebarDashboard = ({ data }: any) => {
   return (
     <div className="h-full mt-8 flex flex-col items-center justify-center">
-      <Search />
+      <Suspense>
+        <Search />
+      </Suspense>
     </div>
   );
 };
