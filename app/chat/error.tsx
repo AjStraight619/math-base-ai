a6eccab4460489022f39bdd5066a44ab36687d1a
Alex Straight <compsciaj@outlook.com>
2024-03-05 14:34:00 -0800

    added use client directive to error page

--- app/chat/error.tsx
+++ app/chat/error.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export default function Error() {
   return (
     <main className="min-h-screen flex flex-col justify-between items-end md:justify-center md:items-center px-8 md:px-12 w-full">
