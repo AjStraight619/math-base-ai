cef5b3b693ba13fa664926e4db52c22c17fe07e2
Alex Straight <compsciaj@outlook.com>
2024-03-11 21:55:10 -0700

    refactoring

--- components/dashboard-page/user-activity-fetcher.tsx
+++ components/dashboard-page/user-activity-fetcher.tsx
@@ -1,19 +1,19 @@
-import { getFoldersWithNotes } from "@/actions/folder-actions";
-import { Suspense } from "react";
-import UserActivity from "./user-activity";
+// import { getFoldersWithNotes } from "@/actions/folder-actions";
+// import { Suspense } from "react";
+// import UserActivity from "./user-activity";
 
-type UserActivityFetcherProps = {
-  query: string;
-};
+// type UserActivityFetcherProps = {
+//   query: string;
+// };
 
-const UserActivityFetcher = async ({ query }: UserActivityFetcherProps) => {
-  const folderData = await getFoldersWithNotes();
+// const UserActivityFetcher = async ({ query }: UserActivityFetcherProps) => {
+//   const folderData = await getFoldersWithNotes();
 
-  return (
-    <Suspense>
-      <UserActivity folderData={folderData} />
-    </Suspense>
-  );
-};
+//   return (
+//     <Suspense>
+//       <UserActivity folderData={folderData} />
+//     </Suspense>
+//   );
+// };
 
-export default UserActivityFetcher;
+// export default UserActivityFetcher;
--- components/dashboard-page/user-activity.tsx
+++ components/dashboard-page/user-activity.tsx
@@ -1,62 +1,62 @@
-"use client";
-import { useSidebarContext } from "@/context/sidebar-presence-context";
-import { FolderWithNotesSnippets, FoldersWithNotes } from "@/lib/types";
-import { createSnippet } from "@/lib/utils";
-import { useEffect, useState } from "react";
-import FolderDisplay from "./folder-display";
+// "use client";
+// import { useSidebarContext } from "@/context/sidebar-presence-context";
+// import { FolderWithNotesSnippets, FoldersWithNotes } from "@/lib/types";
+// import { createSnippet } from "@/lib/utils";
+// import { useEffect, useState } from "react";
+// // import FolderDisplay from "./folder-display";
 
-type UserActivityProps = {
-  folderData: FoldersWithNotes;
-};
+// type UserActivityProps = {
+//   folderData: FoldersWithNotes;
+// };
 
-const UserActivity = ({ folderData }: UserActivityProps) => {
-  const { foldersWithNotes, error: folderDataError } = folderData;
-  const [dataView, setDataView] = useState<"list" | "grid">("grid");
-  const [error, setError] = useState<string | null>(folderDataError);
+// const UserActivity = ({ folderData }: UserActivityProps) => {
+//   const { foldersWithNotes, error: folderDataError } = folderData;
+//   const [dataView, setDataView] = useState<"list" | "grid">("grid");
+//   const [error, setError] = useState<string | null>(folderDataError);
 
-  const [selectedFolder, setSelectedFolder] =
-    useState<FolderWithNotesSnippets | null>(null);
+//   const [selectedFolder, setSelectedFolder] =
+//     useState<FolderWithNotesSnippets | null>(null);
 
-  const foldersWithNotesSnippets: FolderWithNotesSnippets[] =
-    foldersWithNotes?.map((folder) => ({
-      ...folder,
-      notes: folder.notes.map((note) => ({
-        ...note,
-        noteContents: note.noteContents.map((noteContent) => ({
-          ...noteContent,
-          content: createSnippet(noteContent.content),
-        })),
-      })),
-    })) ?? [];
+//   const foldersWithNotesSnippets: FolderWithNotesSnippets[] =
+//     foldersWithNotes?.map((folder) => ({
+//       ...folder,
+//       notes: folder.notes.map((note) => ({
+//         ...note,
+//         noteContents: note.noteContents.map((noteContent) => ({
+//           ...noteContent,
+//           content: createSnippet(noteContent.content),
+//         })),
+//       })),
+//     })) ?? [];
 
-  const { isSidebarOpen } = useSidebarContext();
+//   const { isSidebarOpen } = useSidebarContext();
 
-  const containerStyle = isSidebarOpen ? "ml-48" : "ml-0";
+//   const containerStyle = isSidebarOpen ? "ml-48" : "ml-0";
 
-  useEffect(() => {
-    console.log("Is sidebar open? ", isSidebarOpen);
-  }, [isSidebarOpen]);
+//   useEffect(() => {
+//     console.log("Is sidebar open? ", isSidebarOpen);
+//   }, [isSidebarOpen]);
 
-  return (
-    <div className={`${containerStyle} w-3/4`}>
-      <div className="col-span-1 sm:col-span-2 md:col-span-3 text-3xl font-semibold text-primary-foreground text-center">
-        <span>User Activity</span>
-        <span>
-          {error && (
-            <span className="text-red-500 font-semibold text-center">
-              {error}
-            </span>
-          )}
-        </span>
-      </div>
+//   return (
+//     <div className={`${containerStyle} w-3/4`}>
+//       <div className="col-span-1 sm:col-span-2 md:col-span-3 text-3xl font-semibold text-primary-foreground text-center">
+//         <span>User Activity</span>
+//         <span>
+//           {error && (
+//             <span className="text-red-500 font-semibold text-center">
+//               {error}
+//             </span>
+//           )}
+//         </span>
+//       </div>
 
-      <FolderDisplay
-        dataView={dataView}
-        folderWithNotesSnippets={foldersWithNotesSnippets}
-        selectedFolder={selectedFolder}
-        setSelectedFolder={setSelectedFolder}
-      />
-    </div>
-  );
-};
-export default UserActivity;
+//       <FolderDisplay
+//         dataView={dataView}
+//         folderWithNotesSnippets={foldersWithNotesSnippets}
+//         selectedFolder={selectedFolder}
+//         setSelectedFolder={setSelectedFolder}
+//       />
+//     </div>
+//   );
+// };
+// export default UserActivity;
